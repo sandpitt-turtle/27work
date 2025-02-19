957e8dd141444b189a55530841f917603c1122b4
sandpitt-turtle <wottkowicz@gmail.com>
2025-02-19 16:38:41 -0500

    Up to 'Step 2' in 'Authenticate Component'

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import './App.css';
-
+import {useState} from "react";
+import "./App.css";
 import Authenticate from "./components/Authenticate";
 import SignUpForm from "./components/SignUpForm";
+
 export default function App() {
+  const [token, setToken] = useState(null);
   return (
     <>
       <Authenticate />
